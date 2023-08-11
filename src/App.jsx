a1d2bd376f992a0a7aebb34be8a1885914489cce
ceoraford <ceora.ford@okta.com>
2023-08-11 13:18:06 -0400

    Basic navbar starter

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import './styles/App.css'
-
+import NavBar from "./components/navbar.js"
 function App() {
   return (
     <>
       <h1> Welcome to MeetGreet</h1>
+      <NavBar />
     </>
   )
 }
---
+++ src/components/navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function NavBar() {
+  return (
+    <nav>
+      <ul>
+        <li> MeetGreet</li>
+        <li> About </li>
+        <li> Locations </li>
+
+      </ul>
+    </nav>
+  )
+}
